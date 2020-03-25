6e060b1d0384a18e5c514ba57a661ed686fb5787
wangtao <wangtao@jmfen.cn>
2020-03-25 10:46:22 +0800

    fix: comment 评论插件修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,29 @@
+// let GithubV3=require('@vssue/api-github-v3')
 module.exports = {
   plugins: [
+  // '@vuepress/theme-blog',
+    // '@vssue/api-github-v3',
     [
-      '@vssue/vssue',
+      '@vssue/vuepress-plugin-vssue',
       {
+        locale: 'zh',
+        // 设置要使用的平台 api
+        // api: GithubV3,
+        // baseURL: 'https://github.com',
         // 设置 `platform` 而不是 `api`
         platform: 'github',
 
         // 其他的 Vssue 配置
         owner: 'wangtager',
         repo: 'blog',
-        clientId: 'b3e5558b17c4a52f8d98',
-        clientSecret: '73c0830a0dea90562d950d06846e87bd66393efa'
+        // clientId: 'b3e5558b17c4a52f8d98',
+        // clientSecret: '73c0830a0dea90562d950d06846e87bd66393efa'
+        // 本地
+        clientId:'3e010a9999886c6c6e1c',
+        clientSecret:'9a64800d2be2cab9887bda67177cc2beebf1681c',
+        // Accept: 'application/vnd.github.v3+json'
       }
     ],
-    '@vssue/api-github-v3',
     '@vuepress/back-to-top'
   ],
   base: '/blog/',
@@ -27,6 +37,8 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/logo.jpg' }]
   ],
+  // extend: '@vuepress/theme-blog',
+  // theme: '@vuepress/theme-blog',
   themeConfig: {
     repo: 'wangtager/blog',
     docsDir: 'docs',
