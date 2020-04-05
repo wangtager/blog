7c972e251ab46bd211f7168f60abca48ba3fe1ba
tager <2121190200@qq.com>
2020-04-05 11:47:41 +0800

    fix: 清理package.json的依赖版本号和vssuedemo 一直，locale 注释

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,15 +2,15 @@
 module.exports = {
   plugins: [
     ['@vssue/vuepress-plugin-vssue', {
-      autoCreateIssue: true,
+      // autoCreateIssue: true,
       // Accept: application/vnd.github.v3+json,
-      locale: 'zh',
+      // locale: 'zh',
       platform: 'github',
       owner: 'wangtager',
       repo: 'blog',
       // github
-      clientId: '2e5c7372cce3eff6fdc7',
-      clientSecret: '743792ad67a4be3e975cfcfc201c60a04019ab95'
+      clientId: '4e15385783a44fd246a6',
+      clientSecret: '65132245ae2c6feec05ad0fa64dc916de98243ca'
       // gitee
       // clientId: 'fc59d37d93bbb87ba37d60a38f0e6993804e7fe2926a92e8ed4441e71aeac600',
       // clientSecret: 'd5dd71991ec7573c68a0745e46404624b26ed59fd18048bc996a7a1f0ecf7020'
